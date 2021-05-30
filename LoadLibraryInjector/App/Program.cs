27914d9d93d4079ea64888db5f9c9a7cba0abd9c
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2021-05-31 01:16:03 +0300

    Run as administrator

---
+++ LoadLibraryInjector/App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadLibraryInjector
+{
+	internal class Program
+	{
+		/// <summary>
+		/// Can be invoked using the console.
+		/// First argument: lpWindowName of process. Example: "Counter-Strike Global Offensive".
+		/// Second argument: full dll path to dll.
+		/// </summary>
+		/// <param name="args"></param>
+		internal static void Main(string[] args)
+		{
+
+
+		}
+	}
+}
